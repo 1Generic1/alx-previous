251a12d3b351f67268f37dca1a94f1c2b294eded
kelvin <kelvin4stunt@gmail.com>
2023-02-16 23:35:24 +0100

    add to alx low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*main entry point and always return 0 */
+/* main entry point and always return 0 */
 
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 } 
