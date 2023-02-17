a64025c56dcfcf128aa87a1571b83cfa4c91c591
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-16 20:28:15 -0800

    add to alx low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,4 @@ printf("size of int: %ld byte(s)\n", sizeof(int));
 printf("size of long int: %ld byte(s)\n", sizeof(long));
 printf("size of long long int: %ld byte(s)\n", sizeof(long long));
 printf("size of float: %ld byte(s)\n", sizeof(float));
-printf("size of double: %ld byte(s)\n", sizeof(double));
-printf("size of long double: %ld byte(s)\n", sizeof(long double));
 return (0); }
