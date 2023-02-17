879a8f18a4fc1bfa3e9f262e4395659f9b2b0ab4
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-16 18:59:11 -0800

    add to alx low level programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/*
-* main - entry point
-*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-/* return - always 0 on sucess */
 return (0);
 } 
