cdc139c1a4a7ac34408a116a3ddba82a7ae6a9d8
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-16 19:37:56 -0800

    add to alx low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point of the program
  *
  * Return: 0 on success
@@ -8,4 +8,4 @@ int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-} 
+}
