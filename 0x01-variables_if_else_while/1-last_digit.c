4bbc9b25d65b7679f474e3e49e8ae563c5881de6
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-19 15:15:44 -0800

    low level programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d" n % 10);
+	printf("Last digit of %d", n % 10);
 	if (n > 5)
 	{
 		printf("and is greater than 5\n");
@@ -27,3 +27,4 @@ int main(void)
 		printf("and is less than 6 and not 0\n");
 	}
 	return (0);
+}
