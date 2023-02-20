f7d71865ab8d15e81e15ee972271f75d510142a0
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-20 14:54:44 -0800

    low level programming

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,19 +5,19 @@
  * Return: Always 0 on Success
  */
 int main(void)
-{
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		putchar(i + '0');
-	}
-
-	if (i < 9)
-	{
-		putchar(',');
-		putchar(' ');
-	}
-	putcha('\n');
-		return (0);
-}
+{       
+        int i;
+        
+        for (i = 0; i < 10; i++)
+        {       
+                putchar(i + '0');
+        }       
+        
+        if (i < 9)
+        {       
+                putchar(',');
+                putchar(' ');
+        }       
+        putcha('\n');
+                return (0);
+}   
