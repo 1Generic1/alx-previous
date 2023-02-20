f886be38ce2090d155ea7e35772221481afff36a
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-19 18:39:26 -0800

    low level programming

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <studio.h>
+/*
+ * main - Entry point
+ *
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	int i = 'a'
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			putchar(i);
+		}
+	int i = 'A'
+		for (i = 'A'; i <= 'Z' i++)
+		{
+			putchar(i);
+		}
+	putchar('\n');
+	return (0);
+}
+
