ac1d06474da0898453ab0192376dfe94c3dc2478
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-20 08:11:17 -0800

    low level programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: always 0 on success
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i == 0; i < 10 ; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+		return (0);
+}
