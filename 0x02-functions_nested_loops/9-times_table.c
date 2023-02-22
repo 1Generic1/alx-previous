5b6cf49b62b688fa07e436954fafe1c299fc7d47
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-22 15:43:22 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/*
+ * times table - prints the 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = i * j;
+			if (j == 0)
+			{
+				_putchar('0' + product);
+			}
+			else if (product < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else
+				{
+					_putchar('0' + product / 10);
+					_putchar('0' + product % 10);
+				}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
+				
