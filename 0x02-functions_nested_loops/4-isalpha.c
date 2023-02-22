c36b646bb5440ebfd0aaaa07d7c876dcb3d76a35
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-22 07:44:15 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ * @c: single letter
+ * Return: always 1 if lower or upper case or 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if (i >= 'a' && i <= 'z' && i >= 'A' && i <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
