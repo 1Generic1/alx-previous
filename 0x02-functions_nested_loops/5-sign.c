3d2e570b790dbf63dc31cc100d59708719902d16
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-22 08:43:06 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * @n: is a number
+ * Return: always return either + or - or 0 depends
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n = 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
