90297d04c9d9a9ecab75a8a87f0b55c17422fff3
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-22 12:48:14 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * @n: digit input
+ * Return: Always return value of last digit
+ */
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+	
+	if (ld < 0)
+	{
+		ld = -ld;
+	}
+	_putchar(ld + '0');
+	return (ld);
+}
