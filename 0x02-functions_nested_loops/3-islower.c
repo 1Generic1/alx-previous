d7ecb290a1cb1e9e9d6125873aefd64fa1dbb08c
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-22 06:33:58 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 on success or 1 if otherwise
+ */
+int_islower(int c)
+{
+	int c;
+		if (c >= 'a' && c <= 'a')
+		{
+			return (0);
+		}
+		else
+		{
+			return (1);
+		}
+}
