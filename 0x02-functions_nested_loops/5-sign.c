5fe59b29dccde52cefbb1416cb7a065686b036fd
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-22 08:54:33 -0800

    nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	if (n = 0)
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
@@ -21,4 +21,5 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
+	return (0);
 }
