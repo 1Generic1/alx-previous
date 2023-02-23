703c0ed53ef80bb463682d54ef7b1dff4305f325
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-22 16:12:45 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Add two integers
+ * @one: first number input
+ * @two: second number input
+ * Return: return sum
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}
