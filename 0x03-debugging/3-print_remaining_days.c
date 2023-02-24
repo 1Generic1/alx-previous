d9d0b4e382b3e5ef289bfcfaa810bbb6331f2034
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-23 21:01:36 -0800

    0x03-debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,28 +11,28 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
-    int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int days_left = 0;
-    int i;
+	int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int days_left = 0;
+	int i;
 
-    /* Calculate the number of days left in the year */
-    for (i = month - 1; i < 12; i++)
-    {
-        days_left = days_left +  days_in_month[i];
-    }
+	/* Calculate the number of days left in the year */
+	for (i = month - 1; i < 12; i++)
+	{
+	days_left = days_left +  days_in_month[i];
+	}
 
-    days_left = days_left - day;
+	days_left = days_left - day;
 
-    /* Add an extra day for leap years */
-    if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
-    {
-        if (month = 2 && day == 29)
-        {
-            days_left++;
-        }
-    }
+	/* Add an extra day for leap years */
+	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+	{
+	if (month < 2 && day == 29)
+	{
+	days_left++;
+	}
+	}
 
-    /* Print the results */
-    printf("Day of the year: %d\n", convert_day(month, day));
-    printf("Remaining days: %d\n", days_left);
+	/* Print the results */
+	printf("Day of the year: %d\n", convert_day(month, day));
+	printf("Remaining days: %d\n", days_left);
 }
