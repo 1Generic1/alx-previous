eaaafaa0b4eb17f8f414b190b0b762661eda656f
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-24 18:30:45 -0800

    low level programming

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ * @i: input number
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j <= 10; i++)
+	{
+	for (i = 0; i < 15; i++)
+	{
+		if ( i > 9)
+		{
+	_putchar(i / 10 + '0');
+		}
+	_putchar(i % 10 + '0');
+	}
+	_putchar('\n');
+	}
+}	
