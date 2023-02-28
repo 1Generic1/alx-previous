1ed91f0a5b84bd6be5ae42bab6df2263cff14ecb
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-27 16:43:12 -0800

    git push arrays

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int _strlen - entry poitn of function that returns the length of a string
+ * @s: the string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int l;
+	for (l = 0; s[l] =! '\0'; l++)
+		return (l);
+}
