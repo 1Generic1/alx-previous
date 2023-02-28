1961a57c6131b497d06b6988fdf54f35a070cceb
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-27 17:52:07 -0800

    git push arrays

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - entry point
+ * @s: the string
+ */
+void print_rev(char *s)
+{
+	int j;
+	
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		;
+	}
+	for (j = 0; j > 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
