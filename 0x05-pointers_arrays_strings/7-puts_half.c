9b0782f4ce116aae4409e0e278ed2b7a605fdbc6
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-02-27 19:37:21 -0800

    git push arrays

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: the string
+ */
+void puts_half(char *str)
+{
+	int len = 0, i;
+
+	for (len = 0; str[len] != '0'; len++)
+	{
+		;
+	}
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+		else
+		{
+			for (i = (len + 1)/2; i < len; i++)
+			{
+				_putchar(str[i]);
+			}
+		}
+	_putchar('\n');
+}
