fdda2d1a80070ac5e698433709bd8e6589330c67
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-01 13:51:05 -0800

    strings extention

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: destination string
+ * @src: string to be copied
+ * @n: maximum number of characters to be copied from src
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i<n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
