b344fe063c34baa3363dfe944ab3ac2afbdb64a6
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-01 12:03:02 -0800

    strings extention

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat -  entry point, function that concatenates two strings
+ * @src: the string
+ * @dest: also string
+ * Return: *dest
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	int dest_len = 0, src_len = 0;
+
+	while (dest[dest_len] =! '\0')
+	{
+		dest_len++;
+	}
+	while (src[src_len] != '0')
+	{
+		dest[dest_len] = src[src_len];
+		dest_len++;
+		src_len++;
+	}
+	dest[dest_len] = '\0';
+	return (dest);
+}
+
+
+
