db10cd2d28335e48e281b2b7a30360661aaa6ea3
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-01 13:00:54 -0800

    strings extention

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - Function that concatenates two strings
+ * @dest: destination string
+ * @src: source string 
+ * @n: maximun number of bytes from src to be appended to best
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0; src_len = 0
+
+		while (dest[dest_len] != '\0')
+		{
+			dest++;
+		}
+	while (src_len < n && src[src_len] != '\0')
+	{
+		dest[dest_len + src_len] = src[src_len];
+			dest_len++;
+		src_len++;
+	}
+	dest[dest_len + src_len] = '\0';
+	return (dest);
+}
