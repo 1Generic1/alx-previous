bafdfa9785ff763f9f4b1277a88830e7323ff3af
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-02 15:51:40 -0800

    strings extention

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,20 +3,18 @@
 /**
  * print_number - prints an integer
  * @n: integer
- *
- * Return: integer
  */
 
 void print_number(int n)
 {
 	if (n < 0)
 	{
-		_putchar('-')
+		_putchar('-');
 			n = -n;
 	}
 	if (n >= 10)
 	{
 		_putchar(n / 10);
 	}
-	_putchar(n % 10 + '0')
+	_putchar(n % 10 + '0');
 }
