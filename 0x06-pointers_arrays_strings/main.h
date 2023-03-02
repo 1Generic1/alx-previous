b5af72047a07153f622f6cb46d33eb851e4e36f3
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-02 15:13:51 -0800

    strings extention

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,7 +9,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
-char *rot13(char *src;
+char *rot13(char *src);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 int _putchar(char c);
