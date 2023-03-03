46f120aff4fcab9f1696153652deef019b606e07
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-02 16:41:24 -0800

    strings extention

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,12 +4,14 @@
  * reverse_array - function that reverse_array
  * @a: string to be reversed
  * @n: integer number of element of the array
+ *
  */
+
 void reverse_array(int *a, int n)
 {
 	int i, j, temp;
 
-	for (i = 0, j = n - 1; i < j; i++; j--)
+	for (i = 0, j = n - 1; i < j; i++, j--)
 	{
 		temp = a[i];
 		a[i] = a[j];
