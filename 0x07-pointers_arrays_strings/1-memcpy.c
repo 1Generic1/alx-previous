4b11c433d85a883f0306b8803511104a6451e0c6
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-06 19:59:11 -0800

    more pointers and arrays

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *p = dest;
+	unsigned int i;
 
-	while (n--)
+	for (i = 0; i < n; i++)
 	{
-		*dest++ = *scr++;
+		dest[i] = scr[i];
 	}
 	return p;
 }
