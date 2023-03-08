7794d7623aeae38baecfcb79542c411b5a59f1d4
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-07 17:45:55 -0800

    more pointers and arrays

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: the string where the character is to be located
+ * @c: the character
+ *
+ * Return: return character *c or Null
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return s;
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return s;
+	}
+	return null;
+}
