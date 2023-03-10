341fffe9e1e24622137ee8177d01a7861508ae2d
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-10 13:44:59 -0800

    argc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: number of command line argument in the program
+ * @argv: actual command line argument to the program
+ *
+ * Return: always 0 on success
+ */
+
+int main(int argc, char *argv[]__attribute__((__unused__)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
