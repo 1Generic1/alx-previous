f5453dfbecc8bcc7ead8f0f4e0cfb5cbafcd9b9c
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-14 04:13:44 -0700

    mallock

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,6 +14,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
+	unsigned int i;
 
 	if (size == 0)
 	{
@@ -24,7 +25,6 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	unsigned int i;
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
