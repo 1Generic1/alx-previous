d305c530f75f49e376b69dc5462c014e8a996457
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-15 08:38:19 -0700

    mallock

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,12 +4,20 @@
 
 /**
  * free_grid - function that frees a 2 dimensional grid
- * @grid: grid to be freed
- * @height: height of the grid
+ * @grid: A pointer to the 2D array to be freed
+ * @height: height of the 2D array.
  *
- * Return: 0 on success
+ * Return: Nothing
  */
 
 void free_grid(int **grid, int height)
 {
 
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
