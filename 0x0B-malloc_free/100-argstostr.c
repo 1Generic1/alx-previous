14ab2592a6316a8ddf2e2c5a001f99cfb6fe616f
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-15 08:55:58 -0700

    mallock

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * argstostr - function that concatenates all the argument of your program
+ * @ac: number of command argument in the program
+ * @av: array of command argument line values
+ *
+ * Return: Pointer to new string, NULL if it fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+
