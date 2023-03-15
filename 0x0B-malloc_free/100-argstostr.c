4703c74bfa4a771439e492021d4d9e40d5030cb2
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-15 10:53:45 -0700

    mallock

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,4 +10,33 @@
 
 char *argstostr(int ac, char **av)
 {
+	int i, j, k;
+	int len = 0, total = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			len++;
+		}
+	len++;
+	}
+	str = malloc(len * sizeof(char));
+	for (i = 0, k = 0; i < ac; i++)
+	{
+		for(j + 0; av[i][j]; j++, i++)
+		{
+			str[k] = '\n';
+		}
+		k++;
+	}
+	str[k] = '\0';
+	return (str);
+}
 
