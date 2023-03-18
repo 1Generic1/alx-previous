a26d5a61bcb08e75639abd791ecef56bb05b8b06
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-18 07:39:33 -0700

    more malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,31 +4,29 @@
 
 /**
  * array_range - function that creates an array of integers
- * @min: the minimun value in the array
- * @max: the maximun value in the array
+ * @min: the minimun value of element iin the array
+ * @max: the maximun value of element in the array
  *
  * Return: pointer to the newly created array
  */
 
 int *array_range(int min, int max)
 {
-	int *arr;
+	int *p; int i;
 
 	if (min == max)
 	{
 		return (NULL);
 	}
 
-	p = (int *)malloc(sizeof(int) * (max + 1));
+	p = (int *)malloc(sizeof(int) * (max - min + 1));
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-	for (min = 0; min < max; min++)
+	for (i = 0; i <= max; i++)
 	{
-		;
+			p[i] = min + i;
 	}
-
 	return (p);
 }
-
