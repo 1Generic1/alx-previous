df5da058991dab4caeb1f96840a3e639ab4be8c0
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-21 11:16:45 -0700

    struct and typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_dog - function that prints a struct dog
+ * @d: pointer to dog
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	printf("name: %s\n", d->name != NULL ? d->name : "(nil)");
+	printf("age: %.6f\n", d->age);
+	if (d == NULL)
+	{
+		printf("owner:(nil)\n");
+	}
+	else
+	{
+		printf("owner: %s\n", d->owner);
+	}
+}
