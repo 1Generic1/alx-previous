cea2bdeb5b381a0de8563026b3192a862e2a7821
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-21 09:00:42 -0700

    struct and typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * init_dog - function that initialize a variable of type
+ * @d: pointer to a struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return;
+		d -> name = name;
+		d -> age = age;
+		d -> owner = owner;
+	}
+}
