a6bb5e2909308254bef0e3b7a3e1b8f0b8ba8cfe
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-21 08:23:34 -0700

    struct and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,9 @@
 
 /**
  * struct dog - structure containing description of a dog
+ * @name: pointer to the dog's name
+ * @age: the dog's age
+ * @owner: pointer to the owner's name or contact information
  */
 
 struct dog
