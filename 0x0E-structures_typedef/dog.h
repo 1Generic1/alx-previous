0891927ce55d93160c9a93c7ee90a1791d555597
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-21 10:13:50 -0700

    struct and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,14 +7,14 @@
  * @age: the dog's age
  * @owner: pointer to the owner's name or contact information
  */
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif
