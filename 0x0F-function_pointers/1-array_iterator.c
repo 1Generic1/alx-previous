56ce15c2c000780ef23dd835acd009da0cf79b75
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-22 15:17:14 -0700

    functions

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stddef>
+/**
+ * array_iterator - function that executes a function
+ * @array: the array of the integers to process
+ * @size: the size of the array
+ * @action: a pointer to the function to apply to each element
+ *
+ * return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array != NULL & action !=NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
