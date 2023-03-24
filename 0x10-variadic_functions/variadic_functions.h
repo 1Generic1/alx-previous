524b0cdcdb6f7748c8cbed1be8e3d62f41f59d49
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-24 16:35:22 -0700

    variadic function

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,5 +5,6 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_opcodes(char *start, int n_bytes);
 
 #endif /* VARIADIC_FUNCTIONS_H */
