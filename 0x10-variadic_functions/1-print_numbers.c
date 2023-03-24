944f8fc1dafa1a48207ac409b52a7b9f24eab29c
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-23 17:59:33 -0700

    variadic function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,12 +16,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(args, n);
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
-		if (i != n - 1 && seperator != NULL)
+		if (i != n - 1 && separator != NULL)
 		{
-			printf("%s", seperator);
+			printf("%s", separator);
 		}
 	}
 	va_end(args);
