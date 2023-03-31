164900d036730cce2669a2b3d10562aef2430934
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-03-30 18:05:32 -0700

    linked_lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,12 @@
 #define LIST_H
 
 #include <stddef.h>
-typedef struct list_s list_t;
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+}list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
