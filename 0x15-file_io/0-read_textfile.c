e73ee5823bb147742108718df88e0933ea614fc4
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-04-09 07:29:41 -0700

    file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * read_textfile - function that reads a text file and prints 
+ * it to the POSIX standard output.
+ * @filename: the name of the file to read
+ * @letters: the number of letters to read and print
+ *
+ * Return: the number of letters read and printed, or 0 if an error occurs
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd; /* file descriptor */
+	ssize_t bytes_read = 0;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY); /* this is open the file */
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	bytes_read = read(fd, butter, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+	return (bytes_read);
+}
