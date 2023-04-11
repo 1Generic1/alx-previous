915ba095c0bd6bbbbf4c847b6ec639803c4bd53f
kelvin4stunt@gmail.com <kelvin4stunt@gmail.com>
2023-04-11 06:23:45 -0700

    file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,28 +19,29 @@ int main(int argc, char *argv[])
 	char buffer[BUFFER_SIZE];
 	ssize_t bytes_read;
 	ssize_t bytes_written;
+	int fd_to;
+	int fd_from;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	int fd_from = open(argv[1], O_RDONLY);
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprint(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	int fd_to = open(argv[2], O_WRONLY | O_CREAT |O_TRUNC, 0644);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT |O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit (99);
 	}
-	ssize_t bytes_read;
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 		{
-			ssize_t bytes_written = write(fd_to, buffer, bytes_read);
+			bytes_written = write(fd_to, buffer, bytes_read);
 			if(bytes_written == -1)
 			{
 				dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
@@ -54,12 +55,12 @@ int main(int argc, char *argv[])
 	}
 	if (close(fd_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
 	if (close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fd-to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 	return (0);
